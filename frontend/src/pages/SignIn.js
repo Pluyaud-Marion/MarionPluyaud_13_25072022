b3da7bf1ad48e4736276be3863fad18fe8bf990b
Marion Pluyaud <marion070189@hotmail.fr>
2022-07-25 18:16:47 +0200

    implementation user page

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const SignIn = () => {
     return (
-        <div>
-            <h1>Page sign in</h1>
+        <div className='SignIn'>
+            <section className='container-signin'>
+                <i className="fas fa-user-circle"></i>
+                <h1>Sign In</h1>
+                <form action="" className='form'>
+                    <div className='input'>
+                        <label className='label-input' htmlFor="username">Username</label>
+                        <input type="text" id='username' />
+                    </div>
+                    <div className='input'>
+                        <label className='label-input' htmlFor="password">Password</label>
+                        <input type="text" id='password' />
+                    </div>
+                    <div className='input-remember'>
+                        <input type="checkbox" id='remember-me' />
+                        <label className='label-remember-me' htmlFor="remember-me">Remember me</label>
+                    </div>
+                    <NavLink to="/user" className="button-signin">Sign In</NavLink>
+                </form>
+            </section>
         </div>
     );
 };
