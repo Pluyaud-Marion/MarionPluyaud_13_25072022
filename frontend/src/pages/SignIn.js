bd3bc322ad93ea8341b5e25f9dc44e100301fa40
Marion Pluyaud <marion070189@hotmail.fr>
2022-08-02 18:13:14 +0200

    implementation callApi for take token

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,7 +1,48 @@
+import axios from 'axios';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { Navigate, NavLink } from 'react-router-dom';
+import { getToken } from '../utils/services/getToken';
+import { setUsers } from "../feature/users.slice"
 
 const SignIn = () => {
+    // let email = ""
+    // let password = ""
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const urlApi = "http://localhost:3001/api/v1/user"
+
+    const dispatch = useDispatch();
+
+    const login = {
+        email,
+        password
+    }
+
+    const submitLogin = (e) => {
+        e.preventDefault()
+        axios.post(urlApi + "/login", login)
+            .then(res => {
+                console.log(res.data);
+                dispatch(setUsers(res.data.body))
+                if (res.data.body) {
+                    return <Navigate to="/user" />
+                }
+            }
+                //return <Navigate to="/user" />
+
+
+            )
+
+    }
+
+
+    //useGetToken()
+
+
+
     return (
         <div className='SignIn'>
             <section className='container-signin'>
@@ -10,21 +51,30 @@ const SignIn = () => {
                 <form action="" className='form'>
                     <div className='input'>
                         <label className='label-input' htmlFor="username">Username</label>
-                        <input type="text" id='username' />
+                        <input onChange={(e) => setEmail(e.target.value)} type="text" id='username' />
+                        {/* <p className='noEmail'></p> */}
                     </div>
                     <div className='input'>
                         <label className='label-input' htmlFor="password">Password</label>
-                        <input type="text" id='password' />
+                        <input onChange={(e) => setPassword(e.target.value)} type="text" id='password' />
+                        {/* <p className='noPassword'></p> */}
                     </div>
                     <div className='input-remember'>
                         <input type="checkbox" id='remember-me' />
                         <label className='label-remember-me' htmlFor="remember-me">Remember me</label>
                     </div>
-                    <NavLink to="/user" className="button-signin">Sign In</NavLink>
+                    {/* <NavLink to="/user" className="button-signin">Sign In</NavLink> */}
+                    {/* <p className='userNotFound'></p> */}
+
+                    <div className='button-signin'>
+                        <button onClick={submitLogin} type="sumbit">Sign In</button>
+                    </div>
+
                 </form>
             </section>
         </div>
     );
+
 };
 
 export default SignIn;
---
+++ frontend/src/utils/services/getToken.js
@@ -0,0 +1,23 @@
+// import axios from "axios"
+// import { useDispatch } from "react-redux"
+// import { setUsers } from "../../feature/users.slice"
+
+
+// export const getToken = (email, password) => {
+//     const dispatch = useDispatch()
+
+//     const urlApi = "http://localhost:3001/api/v1/user"
+
+//     // const login = {
+//     //     "email": "tony@stark.com",
+//     //     "password": "password123"
+//     // }
+//     const login = {
+//         email: "steve@rogers.com",
+//         password: "password456"
+//     }
+
+
+//     axios.post(urlApi + "/login", login)
+//         .then(res => dispatch(setUsers(res.data.body)))
+// }
